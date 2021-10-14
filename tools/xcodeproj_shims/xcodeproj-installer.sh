dbdf1bfff19665fde1d6b43e087a2aee0e6a6e91
mcfans <mcfans@icloud.com>
2021-10-14 14:42:24 +0800

    fix: use new build system

--- tools/xcodeproj_shims/xcodeproj-installer.sh
+++ tools/xcodeproj_shims/xcodeproj-installer.sh
@@ -22,41 +22,11 @@ mkdir -p "${installers_dir}"
 readonly print_json_installers_dir="${stubs_dir}/print_json_leaf_nodes.runfiles/"
 mkdir -p "${print_json_installers_dir}"
 
-# Project runfile installation: these scripts install various runfiles into the
-# project
-for PRINT_INSTALLER_PATH in $(print_json_leaf_nodes_runfiles)
-do
-  mkdir -p "${print_json_installers_dir}/$(dirname $PRINT_INSTALLER_PATH)"
-  cp "${PWD}/../$PRINT_INSTALLER_PATH" "${print_json_installers_dir}/$PRINT_INSTALLER_PATH"
-done
-
-for INSTALLER_PATH in $(installer_runfile_short_paths)
-do
-  cp "$INSTALLER_PATH" "${installers_dir}/"
-done
-cp "$(installer_short_path)" "${installers_dir}/"
-
-build_wrapper_runfile_short_paths="$(build_wrapper_runfile_short_paths)"
-for BUILD_WRAPPER_PATH in $build_wrapper_runfile_short_paths
-do
-  cp -r "$BUILD_WRAPPER_PATH" "${stubs_dir}/"
-done
-
-cp "$(clang_stub_short_path)" "${stubs_dir}/clang-stub"
-cp "$(clang_stub_ld_path)" "${stubs_dir}/ld-stub"
-cp "$(clang_stub_swiftc_path)" "${stubs_dir}/swiftc-stub"
-cp "$(index_import_short_path)" "${installers_dir}/index-import"
-cp "$(print_json_leaf_nodes_path)" "${stubs_dir}/print_json_leaf_nodes"
-cp "$(infoplist_stub)" "${stubs_dir}/Info-stub.plist"
-cp "$(build_wrapper_path)" "${stubs_dir}/build-wrapper"
-cp "$(output_processor_path)" "${stubs_dir}/output-processor.rb"
-
 # The new build system leaves a subdirectory called XCBuildData in the DerivedData directory which causes incremental build and test attempts to fail at launch time.
 # The error message says "Cannot attach to pid." This error seems to happen in the Xcode IDE, not when the project is tested from the xcodebuild command.
 # Therefore, we force xcode to use the legacy build system by adding the contents of WorkspaceSettings.xcsettings to the generated project.
 mkdir -p "$tmp_dest/project.xcworkspace/xcshareddata/"
-cp "$(workspacesettings_xcsettings_short_path)" "$tmp_dest/project.xcworkspace/xcshareddata/"
-cp "$(ideworkspacechecks_plist_short_path)" "$tmp_dest/project.xcworkspace/xcshareddata/"
+# cp "$(workspacesettings_xcsettings_short_path)" "$tmp_dest/project.xcworkspace/xcshareddata/"
 
 chmod -R +w "${tmp_dest}"
 
